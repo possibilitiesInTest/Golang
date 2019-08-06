c892ec7eb057a980f33bee3d4d2ff81ff45462ce
Gourav B <pit.wire@pm.me>
2019-08-06 16:00:36 -0400

    iterating through two slices: returning each cardValue of type cardSuit

--- src/CARDS/deck.go
+++ src/CARDS/deck.go
@@ -7,7 +7,18 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
-	cars := deck{"Ace of Spades", "Two of Spades"}
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
 }
 
 func (myDeck deck) print() {
--- src/CARDS/main.go
+++ src/CARDS/main.go
@@ -1,14 +1,7 @@
 package main
 
 func main() {
-	// var card = "Ace of Spades"
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
